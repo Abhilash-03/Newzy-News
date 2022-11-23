6401651ff939a4806c2bc5a410acde7810fa67bf
Abhilash <abhilashjha335@gmail.com>
2022-11-23 22:51:50 +0530

    styling in badges

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -9,14 +9,15 @@ export class NewsItems extends Component {
           className="card"
           style={{ border: "2px solid black" }}
         >
-           <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{
-            zIndex: "1", left: "16%"
-           }}>
-    {source} </span>
+          <div style={{
+            display: 'flex', justifyContent: "flex-end", position: "absolute"
+          }}>
+           <span className=" badge rounded-pill bg-danger">{source} </span>
+    </div>
           <img src={!imageurl?"https://wallpapercave.com/wp/wp7342177.jpg": imageurl} className="card-img-top" alt="..." height={"150px"} width={"100px"}/>
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{!description?title:description}...</p>
+            <h5 className="card-title h-threedots">{title}</h5>
+            <p className="card-text p-threedots">{!description?title:description}...</p>
             <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
             <a rel="noreferrer" href={newsurl}  target="_blank" className="btn btn-sm btn-dark">
               Read more
