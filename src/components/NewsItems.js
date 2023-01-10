6cf82e59baf5d9771cb5761090162ef419f9720b
Abhilash <abhilashjha335@gmail.com>
2023-01-10 22:03:04 +0530

    few styling

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -14,14 +14,18 @@ const NewsItems = (props)=> {
            <span className=" badge rounded-pill bg-danger">{source} </span>
     </div>
           <img src={!imageurl?"https://wallpapercave.com/wp/wp7342177.jpg": imageurl} className="card-img-top" alt="..." height={"150px"} width={"100px"}/>
-          <div className="card-body">
+          <div className="card-body" style={{height: "292px"}}>
             <h5 className="card-title h-threedots">{title}</h5>
-            <p className="card-text">{!description?title:description}...</p>
-            <p className="card-text">By -<small className="text-muted"><b> {!author?"Unknown":author}</b></small></p>
+            <p className="card-text" style={{height: '70px'}}>{!description?title:description}...</p>
+            <div className="card-text" style={{paddingTop: '1.8rem'}}>
+            <p className="card-text">By -<small className="text-muted"><b> {!author?"Unknown Author":author}</b></small></p>
             <p className="card-text">Date:<small className="text-muted"><b> {new Date(date).toGMTString()}</b></small></p>
+            </div>
+            <div className="text-end" style={{paddingTop: '1rem'}} >
             <a rel="noreferrer" href={newsurl}  target="_blank" className="btn btn-sm btn-dark">
               Read more
             </a>
+            </div>
           </div>
         </div>
        
