7d495d8a2a0e198ab90ca271781409d173a7cc85
Abhilash <abhilashjha335@gmail.com>
2023-01-11 01:05:37 +0530

    add media query

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -13,15 +13,15 @@ const NewsItems = (props)=> {
           }}>
            <span className=" badge rounded-pill bg-danger">{source} </span>
     </div>
-          <img src={!imageurl?"https://wallpapercave.com/wp/wp7342177.jpg": imageurl} className="card-img-top" alt="..." height={"150px"} width={"100px"}/>
+          <img src={!imageurl?"https://wallpapercave.com/wp/wp7342177.jpg": imageurl} className="card-img-top" alt="..." height={"150px"} width={"150px"}/>
           <div className="card-body" style={{height: "292px"}}>
             <h5 className="card-title h-threedots">{title}</h5>
-            <p className="card-text" style={{height: '70px'}}>{!description?title:description}...</p>
-            <div className="card-text" style={{paddingTop: '1.8rem'}}>
+            <p className="card-text cText" style={{height: '70px'}}>{!description?title:description}...</p>
+            <div className="card-text cText" style={{paddingTop: '1.8rem'}}>
             <p className="card-text">By -<small className="text-muted"><b> {!author?"Unknown Author":author}</b></small></p>
             <p className="card-text">Date:<small className="text-muted"><b> {new Date(date).toGMTString()}</b></small></p>
             </div>
-            <div className="text-end" style={{paddingTop: '1rem'}} >
+            <div className="text-end" style={{paddingTop: '10px'}} >
             <a rel="noreferrer" href={newsurl}  target="_blank" className="btn btn-sm btn-dark">
               Read more
             </a>
